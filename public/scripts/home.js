7c248228eee02ff89904a0867037c2c9f10b14ce
Akhilesh Sharma <akhilsharma3332@gmail.com>
2024-12-20 20:15:55 +0530

    [feature] contact form completed

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -29,3 +29,41 @@ function submitOnlyPhoneNumber(event){
         alert(error);
     });
 }
+
+
+function submitQueryForm(event){
+    event.preventDefault(); // preventing page to reload
+
+    const name  = document.getElementById("name").value;  
+    const phone = document.getElementById("phone").value;  
+    const email = document.getElementById("email").value;  
+    const msg = document.getElementById("msg").value;  
+    
+    console.log(phone);
+    
+    // Make a POST request to the "api/submit" endpoint
+    fetch('/api/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', 
+            "name": name,
+            "phone": phone,
+            "email": email,
+            "msg": msg
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json(); // Process the response if it's successful
+        }
+        throw new Error('Please check the number');
+    })
+    .then(response => {
+        console.log('Success:', response); // Handling the success response
+        alert(response.message);
+    })
+    .catch(error => {
+        console.error('Error:', error); // Handling errors
+        alert(error);
+    });
+}
