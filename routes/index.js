dff3601556bda0009234f9bcc4143a05b9c6cfd8
Akhilesh Sharma <akhilsharma3332@gmail.com>
2024-12-30 00:22:54 +0530

    [New Pages Added] Routes Added bedrooms, halls, kitchens, paintings, wardrobes

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,28 @@ router.get('/about', (req, res) => {
   res.render('pages/about');
 });
 
+router.get('/bedrooms', (req, res) => {
+  res.render('pages/bedrooms');
+});
+
 router.get('/interiors', (req, res) => {
   res.render('pages/interiors');
+});
+
+router.get('/halls', (req, res) => {
+  res.render('pages/halls');
+});
+
+router.get('/kitchens', (req, res) => {
+  res.render('pages/kitchens');
+});
+
+router.get('/paintings', (req, res) => {
+  res.render('pages/paintings');
+});
+
+router.get('/wardrobes', (req, res) => {
+  res.render('pages/wardrobes');
 });
 
 module.exports = router;
