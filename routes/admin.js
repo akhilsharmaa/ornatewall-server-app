cfd13469fe3ef72967c35b761520a72b10ca1f8f
Akhilesh Sharma <akhilsharma3332@gmail.com>
2025-01-11 23:41:37 +0530

    [feature: archived] working

--- routes/admin.js
+++ routes/admin.js
@@ -30,4 +30,26 @@ router.get('/inbox', async (req, res) => {
 }); 
 
 
+router.get('/archived', async (req, res) => {
+
+  try{
+      const result = await RecordModel.find(
+        {label: "archived"}  
+      );
+      res.status(200).render('pages/admin/archived', 
+         {"result": result}
+      );
+
+  }catch(err){
+    console.log(err);
+    return res.status(500).send({
+        "message": "Failed to fetch records, please try again later. ", 
+        "error" : err
+      }
+    );
+  }
+
+}); 
+
+
 module.exports = router;
